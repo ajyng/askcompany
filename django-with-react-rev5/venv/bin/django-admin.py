e7efac723a18f6b74e4dc54b0a32ebc0354c0938
ajyng <zxzimin@naver.com>
2021-02-19 03:55:13 +0900

    기본 환결 설정

---
+++ django-with-react-rev5/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ajyng/Desktop/askcompany/django-with-react-rev5/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
