a68a25457a25c133a1230d15ebb5ab0348370d26
ajyng <zxzimin@naver.com>
2021-02-21 02:51:31 +0900

    [feat] 회원가입 API 구현

--- django-with-react-rev5/backend/backend/urls.py
+++ django-with-react-rev5/backend/backend/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('instagram.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', include('instagram.urls')),
 ]
 
 if settings.DEBUG:
